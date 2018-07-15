a91a956fbf548cb8a4b8b6d20157321df85a97e6
soulnix <maximklimenko95@gmail.com>
2018-07-15 20:04:24 +0300

    Fixed only python2 support

--- installvim8.sh
+++ installvim8.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 sudo apt remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common -q -y
- 
-sudo apt install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev  -q -y
+
+sudo apt autoremove -y
+
+sudo apt install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev python3-dev -q -y
 
 #Optional: so vim can be uninstalled again via `dpkg -r vim`
 sudo apt install checkinstall
@@ -10,6 +12,7 @@ sudo apt install checkinstall
 sudo rm -rf /usr/local/share/vim /usr/bin/vim
 
 cd ~/.scripts
+#rm -rf ~/.scripts/vim/
 git clone https://github.com/vim/vim
 cd vim
 git pull && git fetch
@@ -22,9 +25,8 @@ cd ..
 ./configure --prefix=/usr/local --enable-multibyte \
 --enable-perlinterp \
 --enable-rubyinterp \
---enable-pythoninterp=dynamic \
+--enable-pythoninterp \
 --enable-python3interp \
---with-python3-command \
 --enable-luainterp \
 --with-luajit \
 --enable-cscope \
@@ -37,3 +39,5 @@ cd ..
 --with-compiledby="Max"
 
 make && sudo make install
+
+rm -rf ~/.scripts/vim/
